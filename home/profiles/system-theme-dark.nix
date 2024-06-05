3fd3ddec483abc18ae9df2827d3fa818625dbc3e
Ellis Rahhal <erahhal@netflix.com>
2024-06-05 11:37:57 -0700

    fixed hyprland tab bar font size

--- home/profiles/system-theme-dark.nix
+++ home/profiles/system-theme-dark.nix
@@ -379,8 +379,8 @@ in
     "col.border_inactive" = "rgba(2b2b2bff)";
     groupbar = {
       font_family = "DejaVu Sans";
-      font_size = 18;
-      height = 18;
+      font_size = 10;
+      height = 22;
       text_color = "rgba(ffffffff)";
       "col.active" = "rgba(285577ff)";
       "col.inactive" = "rgba(2b2b2bff)";
