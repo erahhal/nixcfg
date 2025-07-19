076c5332540cf77e0bb6d30d5075ff6892873d9b
Ellis Rahhal <ellis@rahh.al>
2025-07-19 16:42:16 -0700

    cleaned up hyprland config for p16

--- hosts/nflx-erahhal-p16/launch-apps-config-hyprland.nix
+++ hosts/nflx-erahhal-p16/launch-apps-config-hyprland.nix
@@ -37,18 +37,9 @@ in
       };
 
       bindl = [
-        # trigger when the switch is toggled
+        ## Doesn't suspend or wake up if these aren't added
         '', switch:on:[Lid Switch], exec, "${hyprlockCommand}"''
-        # trigger when the switch is turning on
-        # '', switch:on:[switch name], exec, hyprctl keyword monitor "eDP-1, disable"''
-        # trigger when the switch is turning off
         '', switch:off:[Lid Switch], exec, ${hyprctl} keyword monitor "eDP-1, 3840x2400"''
-        # (
-        #   if osConfig.hostParams.desktop.defaultLockProgram == "swaylock" then
-        #     "$mod_SHIFT, S, exec, nag-graphical 'Suspend?' '${swayLockCommand} suspend'"
-        #   else
-        #     "$mod_SHIFT, S, exec, nag-graphical 'Suspend?' '${hyprlockCommand} suspend'"
-        # )
       ];
 
       monitor = [
