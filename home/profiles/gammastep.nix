970a4f3474fea7e8e4cf32345f5612a3969df4aa
Ellis Rahhal <erahhal@netflix.com>
2025-11-14 09:35:24 -0800

    updated gammastep to automatically guess location

--- home/profiles/gammastep.nix
+++ home/profiles/gammastep.nix
@@ -1,12 +1,12 @@
 {
   services.gammastep = {
     enable = true;
-    provider = "manual";
-    # provider = "geoclue2";
-    latitude = "34.0522";
-    longitude = "-118.2437";
+    ## latitude = "34.0522";
+    # provider = "manual";
+    provider = "geoclue2";
+     longitude = "-118.2437";
     temperature.day = 6500;
-    temperature.night = 4500;
+    temperature.night = 3600;
     tray = true;
   };
 }
