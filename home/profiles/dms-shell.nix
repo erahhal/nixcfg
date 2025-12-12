c1c6c3e2c74060a3bd2bd6fb10e50f56e717d6eb
Ellis Rahhal <erahhal@netflix.com>
2025-12-11 18:04:43 -0800

    dms tweaks

--- home/profiles/dms-shell.nix
+++ home/profiles/dms-shell.nix
@@ -1,4 +1,4 @@
-{ inputs, pkgs, config, ... }:
+{ inputs, pkgs, config, lib, ... }:
 {
   home.file."Wallpaper".source = ../../wallpapers;
 
@@ -6,6 +6,26 @@
   systemd.user.services.dms.Service.Environment = [
     "PATH=${config.home.profileDirectory}/bin:/run/current-system/sw/bin"
   ];
+
+  # Sync default-settings.json to settings.json on activation
+  home.activation.dmsSettingsSync = lib.hm.dag.entryAfter ["writeBoundary"] ''
+    DMS_CONFIG_DIR="${config.xdg.configHome}/DankMaterialShell"
+    DEFAULT_SETTINGS="$DMS_CONFIG_DIR/default-settings.json"
+    SETTINGS="$DMS_CONFIG_DIR/settings.json"
+
+    if [ -f "$DEFAULT_SETTINGS" ]; then
+      mkdir -p "$DMS_CONFIG_DIR"
+      if [ -f "$SETTINGS" ]; then
+        # Deep merge: default-settings.json values override settings.json
+        ${pkgs.jq}/bin/jq -s '.[0] * .[1]' "$SETTINGS" "$DEFAULT_SETTINGS" > "$SETTINGS.tmp"
+        cat "$SETTINGS.tmp" > "$SETTINGS"
+        rm "$SETTINGS.tmp"
+      else
+        # No settings.json exists, just copy default-settings.json
+        cat "$DEFAULT_SETTINGS" > "$SETTINGS"
+      fi
+    fi
+  '';
 
   programs.dankMaterialShell = {
     enable = true;
@@ -30,9 +50,24 @@
         position = 1;  # 0=top, 1=bottom, 2=left, 3=right
         screenPreferences = ["all"];
         showOnLastDisplay = true;
-        leftWidgets = ["launcherButton" "workspaceSwitcher" "focusedWindow"];
+        leftWidgets = [
+          "launcherButton"
+          "workspaceSwitcher"
+          "focusedWindow"
+        ];
         centerWidgets = [];
-        rightWidgets = ["music" "systemTray" "clipboard" "cpuUsage" "memUsage" "notificationButton" "battery" "controlCenterButton" "weather" "clock"];
+        rightWidgets = [
+          "music"
+          "systemTray"
+          "clipboard"
+          "cpuUsage"
+          "memUsage"
+          "battery"
+          "controlCenterButton"
+          "weather"
+          "clock"
+          "notificationButton"
+        ];
         spacing = 4;
         innerPadding = 4;
         bottomGap = 0;
