8515c4760957cbfae5d346a3e6880028c7927e64
Ellis Rahhal <ellis@rahh.al>
2026-01-09 22:31:31 -0800

    msi-linux tweaks

--- hosts/msi-linux/configuration.nix
+++ hosts/msi-linux/configuration.nix
@@ -1,4 +1,4 @@
-{ config, inputs, pkgs, userParams, ... }:
+{ config, inputs, lib, pkgs, userParams, ... }:
 {
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
@@ -40,6 +40,9 @@
       # ../../profiles/smb-mounts.nix
       ../../profiles/kdeconnect.nix
     ];
+
+  # Disable gnome-keyring since we use autologin and don't need password storage
+  services.gnome.gnome-keyring.enable = lib.mkForce false;
 
   # Needed to setup passwords
   users.users.root.openssh.authorizedKeys.keys = [
--- hosts/msi-linux/kanshi.nix
+++ hosts/msi-linux/kanshi.nix
@@ -4,6 +4,7 @@ let
   tv = "LG Electronics LG TV SSCR2 0x01010101";
   yamaha = "Yamaha Corporation * *";
   index = "Valve Corporation * *";
+  quarto = "Nvidia * *";
 in
 {
   home-manager.users.${userParams.username} = {
@@ -85,6 +86,30 @@ in
               }
               {
                 criteria = index;
+                status = "disable";
+                mode = "2880x1600";
+                position = "0,0";
+                scale = 1.0;
+              }
+            ];
+            exec = [
+              "${hyprctl} dispatch dpms on | true"
+            ];
+          };
+        }
+        {
+          profile = {
+            name = "tv-quarto";
+            outputs = [
+              {
+                criteria = tv;
+                status = "enable";
+                mode = "3840x2160";
+                position = "0,0";
+                scale = 2.666667;
+              }
+              {
+                criteria = quarto;
                 status = "disable";
                 mode = "2880x1600";
                 position = "0,0";
